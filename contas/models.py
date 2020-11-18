c179d2ba73ca563d6bb86479fe5140833a34c4b3
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2020-11-18 14:11:10 -0300

    controleGastos | Criando os models, Model Categoria

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    dt_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
