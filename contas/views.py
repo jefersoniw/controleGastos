c86622abf3c911c720b3bca5c0abea8323dbf02e
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2020-11-18 13:58:45 -0300

    controleGastos | Criando views, registrando urls e retornando um template

--- contas/views.py
+++ contas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+
+def home(request):
+    now = datetime.datetime.now()
+    # html = "<html><body> Agora são %s. </body></html>" % now
+    # return HttpResponse(html)
+
+    return render(request, 'contas/home.html')
