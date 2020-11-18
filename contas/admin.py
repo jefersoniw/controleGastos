28d54603d6098134332f709d6e3c261ee863d5e4
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2020-11-18 14:42:16 -0300

    controleGastos | Criando a model transacao e registrando no admin.py

--- contas/admin.py
+++ contas/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Categoria
+from .models import Categoria, Transacao
 
 admin.site.register(Categoria)
+admin.site.register(Transacao)
--- contas/models.py
+++ contas/models.py
@@ -4,5 +4,24 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
     def __str__(self):
         return self.nome
+
+
+class Transacao(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    descricao = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacoes = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Transação'
+        verbose_name_plural = 'Transações'
+
+    def __str__(self):
+        return self.descricao
