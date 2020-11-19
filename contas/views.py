3bca151de8fdbe91deb0a7ac3bfd96ad0293f93d
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2020-11-19 11:31:17 -0300

    controleGastos | fazendo read de transacao

--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from .models import Transacao
 
 
 def home(request):
@@ -12,3 +13,9 @@ def home(request):
     # return HttpResponse(html)
 
     return render(request, 'contas/home.html', data)
+
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import home
+from contas.views import home, listagem
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home)
+    path('home/', home),
+    path('', listagem)
 ]
